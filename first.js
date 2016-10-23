d3ff9a089e7422360bb1fd3e11cc0d12a1055676
mona-PC\mona <monalisa12.sahoo@gmail.com>
2016-10-23 17:16:59 +0530

    test file for version control

---
+++ first.js
@@ -0,0 +1,13 @@
+var content="i am working in banglore";
+var str=content.split(" ");
+var str1=[];
+console.log("length of str::"+str.length);
+for(var i=0;i<str.length;i++){
+	str1.push(str[i]);
+}
+
+console.log("str::"+str);
+console.log("str1::"+str1);
+
+//go through the link for string methods
+//http://www.w3schools.com/js/js_string_methods.asp
